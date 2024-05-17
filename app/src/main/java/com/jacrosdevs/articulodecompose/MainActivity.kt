4c7274e2ee934351a1b90ae5d3bb7e523688847c
Ernesto Jacros <mas.info.jacros@gmial.com>
2024-05-17 15:19:58 -0600

    se agrego imagen , texto usando funciones compose y dando estilo con modifier

--- app/src/main/java/com/jacrosdevs/articulodecompose/MainActivity.kt
+++ app/src/main/java/com/jacrosdevs/articulodecompose/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jacrosdevs.articulodecompose.ui.theme.ArticuloDeComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +26,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ArticuloDeComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize()) {
+
                 }
             }
         }
@@ -31,17 +35,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ArticleText(
+    title: String, parrafo1: String,
+    parrafo2: String, modifier: Modifier = Modifier
+) {
+    Text(text = title,
+        fontSize = 24.sp,
+        modifier = Modifier.padding(16.dp))
+    Text(text = parrafo1,
+        textAlign = TextAlign.Justify,
+        modifier = modifier.padding(start = 16.dp, end = 16.dp))
+    Text(text = parrafo2,
+        textAlign = TextAlign.Justify,
+        modifier = modifier.padding(16.dp))
 }
+
+@Composable
+fun Article(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Image(
+            painter = painterResource(id = R.drawable.bg_compose_background),
+            contentDescription = null
+        )
+        ArticleText(
+            title = "Jetpack Compose tutorial",
+            parrafo1 = stringResource(R.string.parrafo_uno_text) ,
+            parrafo2 = stringResource(R.string.parrafo_dos_text) )
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ArticuloDeComposeTheme {
-        Greeting("Android")
+        Article()
     }
 }
